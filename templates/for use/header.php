90062b00fd3d7ff14e722cb51bd11226e983f591
MOHIT TIWARI <mohitt533@gmail.com>
2017-05-09 20:39:08 +0530

    events

---
+++ templates/for use/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Announcements</title>
+    <meta name="description" content="A Bootstrap 4 admin dashboard theme that will get you started. The sidebar toggles off-canvas on smaller screens. This example also include large stat blocks, modal and cards. The top navbar is controlled by a separate hamburger toggle button." />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="generator" content="Codeply">
+
+      <link type="text/css" rel="stylesheet" href="material/materialize/css/materialize.min.css"  media="screen,projection"/>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+  <!-- Material Design Lite -->
+    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <!-- Material Design icon font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+       <link rel="stylesheet" href="nav/style_nav.css">
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+
+    <body>
+	
+	
+	<div class="ct" id="t1">
+ <div class="ct" id="t2">
+   <div class="ct" id="t3">
+     <div class="ct" id="t4">
+<section>  
+ <ul>
+   <a href="#t1"><li class="icon fa fa-home" id="uno"></li></a>
+   <a href="#t2"><li class="icon fa fa-keyboard-o" id="dos"></li></a>
+   <a href="#t3"><li class="icon fa fa-coffee" id="tres"></li></a>
+   <a href="#t4"><li class="icon fa fa-dribbble" id="cuatro"></li></a>
+ </ul>
+  <div class="page" id="p1">
+     <li class="icon fa fa-home"><span class="title">Home</span><span class="hint">Like this pen to see the magic!...<br/> Just kidding, it won't happen anything but I'll be really happy If you do so.</span></li>   
+  </div>
+  <div class="page" id="p2">
+    <li class="icon fa fa-keyboard-o"><span class="title">Type</span></li>
+  </div>  
+  <div class="page" id="p3">
+    <li class="icon fa fa-coffee"><span class="title">Coffee</span></li>
+  </div>
+  <div class="page" id="p4">
+   <li class="icon fa fa-dribbble"><span class="title">Dribbble</span><span class="hint"><a href="https://dribbble.com/rupok" target="_blank">Follow me on Dribbble</span></li>
+  </div>  
+	<p class="credit"> Original Pen by <a href="http://codepen.io/hrtzt/">Alberto Hartzet</a></p>
+</section>
+       
+     </div>
+     </div>
+   </div>
+  </div>
+	
+	</body>
+	</html>
+
+
+
+    
