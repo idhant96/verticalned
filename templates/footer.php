db8438dee85fb7a188bcef7f309406d146dee5a6
coldplayer <idhant123@gmail.com>
2017-05-10 00:55:16 +0530

    footer modified

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,12 @@
 
 <!--Import jQuery before materialize.js-->
      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-     <script type="text/javascript" src="js/materialize.min.js"></script>
-
+    <script type="text/javascript" src="material/materialize/js/materialize.min.js"></script>
+<script>
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('#modal1').modal();
+});
+ </script>
   </body>
 </html>
