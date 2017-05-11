dad07c1a8c34292f353fb30905d5b063febfd882
coldplayer <idhant123@gmail.com>
2017-05-12 03:17:24 +0530

    added collapsable triggers

--- templates/footer.php
+++ templates/footer.php
@@ -7,6 +7,18 @@ $(document).ready(function(){
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('#modal1').modal();
 });
+function expandAll(){
+  $(".collapsible-header").addClass("active");
+  $(".collapsible").collapsible({accordion: false});
+}
+
+function collapseAll(){
+  $(".collapsible-header").removeClass(function(){
+    return "active";
+  });
+  $(".collapsible").collapsible({accordion: true});
+  $(".collapsible").collapsible({accordion: false});
+}
  </script>
   </body>
 </html>
