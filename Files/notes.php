06b088dfd33e33788d0b67ad052cbab9714f8a1b
coldplayer <idhant123@gmail.com>
2017-05-16 16:56:34 +0530

    working on notes

--- Files/notes.php
+++ Files/notes.php
@@ -3,31 +3,4 @@ require 'templates/header.php';
 require 'templates/components/navbar.php';
 require 'templates/components/search.php';
 require 'templates/footer.php';
-
-
-// Attempt search query execution
-try{
-    if(isset($_REQUEST['term'])){
-        // create prepared statement
-        $sql = "SELECT code FROM subject WHERE code LIKE :term";
-        $stmt = $pdo->prepare($sql);
-        $term = $_REQUEST['term'] . '%';
-        // bind parameters to statement
-        $stmt->bindParam(':term', $term);
-        // execute the prepared statement
-        $stmt->execute();
-        if($stmt->rowCount() > 0){
-            while($row = $stmt->fetch()){
-                echo "<p>" . $row['name'] . "</p>";
-            }
-        } else{
-            echo "<p>No matches found";
-        }
-    }
-} catch(PDOException $e){
-    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
-}
-
-// Close connection
-unset($pdo);
 ?>
