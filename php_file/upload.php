dcdb59c1907bc89ee86a7a957975f340332a0c89
MOHIT TIWARI <mohitt533@gmail.com>
2017-06-17 22:39:34 +0530

    upload still not complete

--- php_file/upload.php
+++ php_file/upload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "uploads/";
+$target_dir = "Files/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
@@ -24,11 +24,11 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-// Allow certain file formats
+/* Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+    $uploadOk = 0;*/
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
